e48ad410cab4496ac4184eb4845d6b4acb9a0dba
Lifeng Lin <lifeng@lifengs-mbp.lan>
2022-11-22 21:09:07 -0800

    vivisite 2023 submenu behavior working

--- vivi_site_2023/vue_app/main.js
+++ vivi_site_2023/vue_app/main.js
@@ -2,13 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       message: 'Hello Ha!!',
-      current_section: "home"
+      current_section: "home",
+      hideSubmenu: true,
+      current_project_section: ''
     }
   },
   methods: {
     update_current_section(target_element_id) {
       this.current_section=target_element_id;
-    }
+    },
+    update_current_project_section(target_element_id) {
+      this.current_section="projects";
+      this.current_project_section=target_element_id;
+    },
   }
 })
 
