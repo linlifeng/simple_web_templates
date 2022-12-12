5335dcfd8a2fc651daf627a49848938b32adda3d
linlifeng <linlifeng@gmail.com>
2022-12-11 20:52:34 -0800

    chatgdt generated slidesshow test. index2.html works

---
+++ chatgdp/slideshow.js
@@ -0,0 +1,60 @@
+// Set the initial display styles for the slides
+var slides = document.querySelectorAll(".slide");
+for (var i = 0; i < slides.length; i++) {
+  //slides[i].style.display = "none";
+  slides[i].style.opacity = 0;
+  slides[i].style.transition = "opacity 1s ease-in-out";
+}
+  slides[0].style.opacity = 1;
+
+// Set the initial slide index to 0 (the first slide)
+var slideIndex = 0;
+
+// Show the first slide
+slides[slideIndex].style.display = "block";
+
+// Define a function to advance the slide show to the next slide
+function nextSlide() {
+  // Hide the current slide
+  slides[slideIndex].style.opacity = 0;
+
+  // Increment the slide index
+  slideIndex++;
+
+  // If the slide index is greater than the total number of slides, reset it to 0
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  // Show the next slide
+  slides[slideIndex].style.display = "block";
+  slides[slideIndex].style.opacity = 1;
+}
+
+// Define a function to move the slide show to the previous slide
+function prevSlide() {
+  // Hide the current slide
+  slides[slideIndex].style.opacity = 0;
+
+  // Decrement the slide index
+  slideIndex--;
+
+  // If the slide index is less than 0, set it to the last slide in the slide show
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  // Show the previous slide
+  slides[slideIndex].style.display = "block";
+  slides[slideIndex].style.opacity = 1;
+}
+
+// Define a function to start the slide show
+function startSlideshow() {
+  // Set the interval to call the "nextSlide" function every 5 seconds
+  setInterval(nextSlide, 5000);
+}
+
+// Start the slide show
+// startSlideshow();
+
